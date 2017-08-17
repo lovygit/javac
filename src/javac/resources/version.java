5e8ce08affbcf6d9cc777f22b785fd94f3a020f8
lovymy <15002924939@163.com>
2017-08-17 10:56:41 +0800

    upload the src

---
+++ src/javac/resources/version.java
@@ -0,0 +1,11 @@
+package javac.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "1.8.0_131-8u131-b11-2ubuntu1.16.04.3-b11" },
+            { "jdk", "1.8.0_131" },
+            { "release", "1.8.0_131" },
+        };
+    }
+}
